f1cefb2c4d2bf23be28bc692c4b01319b45be329
Justin Threlkeld <justin.threlkeld@gmail.com>
2015-02-13 10:48:34 -0600

    refactor nested `if` by using `trim()`

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,10 +1,9 @@
 var words = function(text){
+	text.trim()
 	text = text.split(/\s+/g);
 	var words = {};
 	text.forEach(function(word){
-		if (word !== ''){
 			words.hasOwnProperty(word) ? words[word] = words[word] + 1 : words[word] = 1 ;
-		}
 	})
 
 	return words;
