a69d1c9930b52afb08042336175cc185d653db3b
Justin Threlkeld <justin.threlkeld@gmail.com>
2015-02-15 21:36:30 -0600

    hamming optimisations

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -2,21 +2,19 @@ var Hamming = function(){}
 
 Hamming.compute = function(a, b){
 	var delta = 0;
-	if (a === b){
-		return delta;
-	} else if (a.length === b.length) {
+	
+	if (a.length !== b.length) {
+		throw new Error('DNA strands must be of equal length.');
+	} else if (a !== b){
 		a = a.split('');
 		b = b.split('');
-
 		a.forEach(function(val, i){
 			if (a[i] !== b[i]){
 				delta++;
 			}
 		})
-		return delta;
-	} else {
-		throw new Error('DNA strands must be of equal length.');
 	}
+	return delta;
 }
 
 module.exports = Hamming;
