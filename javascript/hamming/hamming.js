6796dceedec9d4dcdd9e5a25de0740b18c199be6
Justin Threlkeld <justin.threlkeld@gmail.com>
2015-03-09 19:28:15 -0500

    check for error first, then instantiate delta

--- javascript/hamming/hamming.js
+++ javascript/hamming/hamming.js
@@ -1,11 +1,11 @@
 var Hamming = function(){}
 
 Hamming.compute = function(a, b){
-	var delta = 0;
-	
 	if (a.length !== b.length) {
 		throw new Error('DNA strands must be of equal length.');
-	} else if (a !== b){
+	}
+	var delta = 0;
+	if (a !== b){
 		a = a.split('');
 		b = b.split('');
 		a.forEach(function(val, i){
